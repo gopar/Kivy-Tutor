41bd9e1b7e56552dd8dce1cf816380f267890e0c
Daniel Gopar <gopardaniel@yahoo.com>
2015-06-06 12:09:12 -0700

    End of fourth video

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from __future__ import division, absolute_import
 from __future__ import print_function, unicode_literals
 
+import webbrowser
+
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
 
@@ -13,6 +15,13 @@ class KivyTutorRoot(BoxLayout):
     def __init__(self, **kwargs):
         super(KivyTutorRoot, self).__init__(**kwargs)
 
+    def changeScreen(self, next_screen):
+        operations = "addition subtraction multiplication division".split()
+        question = None
+
+        if next_screen == "about this app":
+            self.ids.kivy_screen_manager.current = "about_screen"
+
 
 ################################################################################
 class KivyTutorApp(App):
@@ -24,6 +33,24 @@ class KivyTutorApp(App):
 
     def build(self):
         return KivyTutorRoot()
+
+    def getText(self):
+        return ("Hey There!\nThis App was built using"
+                "[b][ref=kivy]kivy[/ref][/b]\n"
+                "Feel free to look at the source code "
+                "[b][ref=source]here[/ref][/b].\n"
+                "This app is under the [b][ref=mit]MIT License[/ref][/b]\n"
+                "My site: [b][ref=website]PyGopar.com[/ref][/b]")
+
+    def on_ref_press(self, instance, ref):
+        _dict = {
+            "source": "https://github.com/gopar/Kivy-Tutor",
+            "website": "http://www.pygopar.com",
+            "kivy": "http://kivy.org/#home",
+            "mit": "https://github.com/gopar/Kivy-Tutor/blob/master/LICENSE"
+        }
+
+        webbrowser.open(_dict[ref])
 
 if __name__ == '__main__':
     KivyTutorApp().run()
